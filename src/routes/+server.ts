d7f50c504e217721eb240917c0ba8e5bf90f0191
zaddone <zaddone@qq.com>
2026-01-27 09:44:12 +0800

    bak

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,5 +1,18 @@
 import { error,json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+
+const tmpCode = new Map<string,string>()
+function generateRandomString(length: number): string {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    result += chars[randomIndex];
+  }
+  
+  return result;
+}
 export const POST:RequestHandler=async (e) => {
     console.log(e)
     await e.platform?.env.KV.put("test","1231")
@@ -9,9 +22,14 @@ export const POST:RequestHandler=async (e) => {
 export const GET: RequestHandler =async ({url, request, platform }) => {
     ///await platform?.env.KV.put("test","1231")
     const k = url.searchParams.get("k")
-    if (k)     
-        return json({msgstruct:await platform?.env.KV.get(k)}) 
-    else 
-        error(404)
-
+    if (k){
+        let code = tmpCode.get(k)
+        if (code){
+            return json({code})
+        }
+        code = generateRandomString(8) 
+        tmpCode.set(k,code)
+        return json({code}) 
+    }
+    error(404)
 };
