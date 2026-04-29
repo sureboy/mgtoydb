0cf9b18ca447012de26a96927ebe8c37e9058e39
zaddone <zaddone@qq.com>
2026-04-29 20:06:53 +0800

    bak

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import cloudflareDoExporter from 'sveltekit-cloudflare-durable-objects';
+//import cloudflareDoExporter from 'sveltekit-cloudflare-durable-objects';
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		cloudflareDoExporter({ durableObjects: ['src/lib/durable-objects.ts'] })
+		//cloudflareDoExporter({ durableObjects: ['src/lib/durable-objects.ts'] })
 	]
 });
